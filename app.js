56b012543bcc045e99e72a46c5f66ad207456c7b
Ma7eer <maher.alkendi2@gmail.com>
2018-10-27 05:16:26 -0600

    added connection to mlab + added movie schema

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+require('dotenv/config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +12,10 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // connect to local mongodb database
-mongoose.connect('mongodb://localhost:27017/movie-planet');
+// mongoose.connect('mongodb://localhost:27017/movie-planet', {useNewUrlParser: true});
+
+// connect to mlab database
+mongoose.connect(process.env.MLAB_URL, {useNewUrlParser: true});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
