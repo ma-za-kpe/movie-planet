ad0ea2fe2563d218a1c87456cbd0c9327bb6dac5
Ma7eer <maher.alkendi2@gmail.com>
2018-10-28 06:03:22 -0600

    added routes protection

--- routes/buyer.js
+++ routes/buyer.js
@@ -1,73 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const passport = require('passport');
-var bcrypt = require('bcrypt-nodejs');
-var Buyer = mongoose.model('Buyer');
+const middleware = require('../middleware/middleware');
 
 // register logic
-router.get('/register', async (req, res, next) => {
+router.get('/register', middleware.isNotLoggedIn, async (req, res, next) => {
     var messages = req.flash('error');
     res.render('register', {
         messages: messages
     });
 });
 
-router.post('/register', passport.authenticate('local-register', {
+router.post('/register', middleware.isNotLoggedIn, passport.authenticate('local-register', {
     successRedirect: '/movies',
     failureRedirect: '/buyers/register',
     failureFlash: true
 }));
 
 // login logic
-router.get('/login', async (req, res, next) => {
+router.get('/login', middleware.isNotLoggedIn, async (req, res, next) => {
     var messages = req.flash('error');
     res.render('login', {
         messages: messages
     });
 });
 
-router.post('/login', passport.authenticate('local-login', {
+router.post('/login', middleware.isNotLoggedIn, passport.authenticate('local-login', {
     successRedirect: '/movies',
     failureRedirect: '/buyers/login',
     failureFlash: true
 }));
 
 //  logout logic
-router.get('/logout', function(req, res, next) {
+router.get('/logout', middleware.isNotLoggedIn, middleware.isLoggedIn, function(req, res, next) {
     req.logout();
     res.redirect('/movies');
   });
-
-
-// register buyers
-// router.get('/register', async(req, res, next) => {
-//     const buyers = await Buyer.find().sort('email')
-//     // res.send(buyers);
-//     res.render('register');
-//   });
-
-// router.post('/register', async(req, res, next) => {
-//     let buyer = await Buyer.findOne({email: req.body.email})
-//     if (buyer) {
-//         return res.status(400).send('Buyer already registered')
-//     }
-//     buyer = new Buyer({
-//         email: req.body.email,
-//         password: req.body.password
-//     })
-//     buyer.password = buyer.encryptPassword();
-//     await buyer.save();
-//     req.flash('success', 'Successfully signed up!')
-//     // res.send(buyer)
-//     res.redirect('/movies');
-// });
-
-// // login buyers
-// router.get('/login', async(req, res, next) => {
-//     const buyers = await Buyer.find().sort('email')
-//     // res.send(buyers);
-//     res.render('login');
-//   });
 
 module.exports = router;
